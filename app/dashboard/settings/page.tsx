85171fe0e8716a0941320d9d5fe60dad222bde9c
kawakami <barcarolle.op60.horowitz@gmail.com>
2024-11-30 23:57:37 +0900

    Add app directory routing and layouts

---
+++ app/dashboard/settings/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SettingPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Setting</h1>
+      <p>This is the setting page.</p>
+    </div>
+  );
+};
+
+export default SettingPage;
