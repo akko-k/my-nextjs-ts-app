ac52765e3150553410db6dc0a58691e4a6a251fe
kawakami <barcarolle.op60.horowitz@gmail.com>
2024-12-17 00:34:10 +0900

    Fetch Todos from Supabase

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,36 @@
-import { Suspense } from 'react';
-import AboutPage from './components/AboutPage';
+'use client';
+import { Tables } from '@/src/types/database.types';
+import { createClient } from '@/utils/supabase/client';
+import { useEffect, useState } from 'react';
 
-export default function HomePage() {
+export default function Home() {
+  const supabase = createClient();
+  const [todos, setTodos] = useState<Tables<'todos'>[]>([]);
+
+  useEffect(() => {
+    const fetchTodos = async () => {
+      const { data, error } = await supabase
+        .from('todos')
+        .select('*');
+      if (error) {
+        console.error(error);
+      }
+
+      setTodos(data ?? []);
+    };
+
+    fetchTodos();
+  }, []);
+
   return (
-    <div>
-      <h1>Welcome to the homepage!</h1>
-      <Suspense fallback={<div>Loading...</div>}>
-        <AboutPage />
-      </Suspense>
-    </div>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <ul>
+        {todos?.map((todo) => (
+          <li key={todo.id} className="text-2xl list-disc">
+            {todo.content}
+          </li>
+        ))}
+      </ul>
+    </main>
   );
 }
